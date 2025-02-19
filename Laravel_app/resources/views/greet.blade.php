2ec3e926945178edbca0381b4c6bbcf87f84a088
XianC <XianCDawg@gmail.com>
2025-02-19 09:33:32 +0000

    Initial Laravel situp with routes, controllers, and views

---
+++ Laravel_app/resources/views/greet.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greet Page</title>
+</head>
+<body>
+    <h1>Hello, welcome to my Laravel project!</h1>
+</body>
+</html>
---
+++ Laravel_app/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/greet', [GreetController::class, 'greet']);
+
+
